bb5dc8f7104cf325a63bf1668fad887000cc5f8d
Linus Bartsch <git@libasoft.de>
2021-05-24 01:35:20 +0200

    add input handling for linux (wip)
    This is not properly working yet :(

--- compatibility.cpp
+++ compatibility.cpp
@@ -61,15 +61,64 @@
 
 // Linux
 #else
+    #include <unistd.h>
+
+    cKeyboard::cKeyboard() {
+        active = false;
+        keyboard_fd = 0;
+        keyboard_ev = new input_event();
+        keyboard_st = new keyboard_state();
+        keyboard_fd = open(KEYBOARD_DEV, O_RDONLY | O_NONBLOCK);
+        if (keyboard_fd > 0) {
+            ioctl(keyboard_fd, EVIOCGNAME(256), name);
+            //std::cout << "   Name: " << name << std::endl;
+            active = true;
+            pthread_create(&thread, 0, &cKeyboard::loop, this);
+        }
+    }
+
+    void* cKeyboard::loop(void *obj) {
+        while (reinterpret_cast<cKeyboard *>(obj)->active)
+            reinterpret_cast<cKeyboard *>(obj)->readEv();
+    }
+
+    void cKeyboard::readEv() {
+        int bytes = read(keyboard_fd, keyboard_ev, sizeof(*keyboard_ev));
+        if (bytes > 0) {
+            if (keyboard_ev->type & EV_KEY) {
+                keyboard_st->keys[keyboard_ev->code] = keyboard_ev->value;
+            }
+        }
+    }
+
+    short cKeyboard::getKeyState(short key) {
+        return keyboard_st->keys[key];
+    }
+
+    cKeyboard::~cKeyboard() {
+        if (keyboard_fd > 0) {
+            active = false;
+            pthread_join(thread, 0);
+            close(keyboard_fd);
+        }
+        delete keyboard_st;
+        delete keyboard_ev;
+        keyboard_fd = 0;
+    }
+
+    cKeyboard kb;
+
     /// Setup Linux console for use
     void setupConsole() {}
 
-    /// Reset color
+    /// Restore console
     void restoreConsole() {
         // Reset colors
         printf("\x1b[0m");
     }
 
-    /// TODO: Gets key state for Linux
-    bool getStateOfKey(int keycode) {}
+    /// Gets key state for Linux
+    bool getStateOfKey(int keycode) {
+        return kb.getKeyState((short) keycode) > 0;
+    }
 #endif
